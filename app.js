60c7e58b30b7475bdcf7301b2a1ffaf3e09ace54
dexterousdeven <devenchoi@Macintosh-414.home>
2014-11-08 20:00:09 -0800

    Added text prompt

--- app.js
+++ app.js
@@ -1,26 +1,78 @@
 $(document).ready(function()
 	{
-		//Number rules
-		for (var i = 1; i < 101; i++)
+		//function to prompt user for number
+		var number = prompt("Please enter a number");
+
+		var fizzBuzz = function(number) 
 		{
-			if (i % 3 === 0)
+		for (var i = 1; i <= number; i++)
 			{
-				$(".list").append('<li>fizz</li>');
-			}
+			if (i % 3 === 0)
+				{
+					$(".list").append('<li>fizz</li>');
+				}
 
 			else if (i % 5 === 0)
-			{
-				$(".list").append('<li>buzz</li>');
-			}
+				{
+					$(".list").append('<li>buzz</li>');
+				}
 
 			else if (i % 5 === 0 && i % 3 === 0)
-			{
-				$(".list").append('<li>fizz buzz</li>');
-			}
+				{
+					$(".list").append('<li>fizz buzz</li>');
+				}
 
 			else
-			{
-				$(".list").append('<li>' + i + '</li>');
-			}
+				{
+					$(".list").append('<li>' + i + '</li>');
+				}
+			};
 		};
+
+		fizzBuzz(number);
+
 	});
+
+		/*prompt(number)
+		{
+			var number = +"number";
+			return number;
+			console.log(number);
+
+			if (number % 1 != 0)
+				{
+					alert("if");
+					fizzBuzz("number");
+				}
+
+			else 
+				{
+					alert("Please enter whole numbers only");
+				}
+		*/
+		//Number rules
+		/*var fizzBuzz = function(number) 
+		{
+		for (var i = number; i < 101; i++)
+			{
+			if (i % 3 === 0)
+				{
+					$(".list").append('<li>fizz</li>');
+				}
+
+			else if (i % 5 === 0)
+				{
+					$(".list").append('<li>buzz</li>');
+				}
+
+			else if (i % 5 === 0 && i % 3 === 0)
+				{
+					$(".list").append('<li>fizz buzz</li>');
+				}
+
+			else
+				{
+					$(".list").append('<li>' + i + '</li>');
+				}
+			};
+		};*/
